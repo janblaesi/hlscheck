418166054c25d8511ba3a836b610de0fbb6ed1f4
Jan Blaesi <jan@blaesi.com>
2025-06-19 18:33:42 +0200

    allow redirecting log to file

--- main.go
+++ main.go
@@ -12,13 +12,18 @@ import (
 
 func main() {
 	var streamUrl string
+	var logPath string
 
 	flag.StringVar(&streamUrl, "url", "", "URL of the stream to check (to the master playlist)")
+	flag.StringVar(&logPath, "logfile", "", "Log file to redirect the output of the program to")
 	flag.Parse()
 
 	if streamUrl == "" {
 		slog.Error("Missing stream URL!")
 		os.Exit(1)
+	}
+	if logPath != "" {
+		SetupLogfile(logPath)
 	}
 
 	StartPlaylistChecker(streamUrl)
@@ -27,6 +32,18 @@ func main() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 	<-signals
+}
+
+// SetupLogfile will add a new slog handler to write the log output to file.
+func SetupLogfile(logPath string) {
+	logFileHandle, err := os.OpenFile(logPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		slog.Error("Unable to open log file", "err", err)
+		os.Exit(1)
+	}
+
+	logHandle := slog.New(slog.NewTextHandler(logFileHandle, nil))
+	slog.SetDefault(logHandle)
 }
 
 // StartPlaylistChecker will fetch the user-provided URL and start a checker instance for each variant playlist.
