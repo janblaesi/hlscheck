cfe18452a0be5f5f25085699301b07cf5fb8c2c4
Jan Blaesi <jan@blaesi.com>
2025-06-19 18:39:16 +0200

    add license file

--- plist/plist.go
+++ plist/plist.go
@@ -1,3 +1,24 @@
+/*
+	Copyright 2025 Jan Blaesi
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+	and associated documentation files (the “Software”), to deal in the Software without
+	restriction, including without limitation the rights to use, copy, modify, merge, publish,
+	distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or
+	substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 package plist
 
 import (
@@ -161,6 +182,8 @@ func Parse(pl *Plist, plUrlStr string, str string) error {
 					return fmt.Errorf("line %d: %v", lineIdx, err)
 				}
 			} else if strings.HasPrefix(extTag, "EXT-X-MEDIA-SEQUENCE") {
+				// We store the media sequence indicated in the playlist to get the unique segment number for each
+				// segment. This can then be used to ensure every segment is only fetched once.
 				if err = parseMediaSequenceTag(pl, extTag); err != nil {
 					return fmt.Errorf("line %d: %v", lineIdx, err)
 				}
